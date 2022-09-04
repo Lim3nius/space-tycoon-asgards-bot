f1218a0f5e86bad93b412ea8c1a7040ffc847188
Ales Franek <ales.franek@gmail.com>
2022-09-05 01:04:39 +0200

    greedy trading

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import random
 import traceback
+from collections import defaultdict
 from collections import Counter
 from pprint import pprint
 from typing import Dict
@@ -15,6 +16,7 @@ from space_tycoon_client.models.data import Data
 from space_tycoon_client.models.destination import Destination
 from space_tycoon_client.models.end_turn import EndTurn
 from space_tycoon_client.models.move_command import MoveCommand
+from space_tycoon_client.models.trade_command import TradeCommand
 from space_tycoon_client.models.player import Player
 from space_tycoon_client.models.player_id import PlayerId
 from space_tycoon_client.models.ship import Ship
@@ -83,15 +85,69 @@ class Game:
             f"{k}:{v}" for k, v in ship_type_cnt.most_common())
         print(f"I have {len(my_ships)} ships ({pretty_ship_type_cnt})")
 
+        sells = defaultdict(dict)
+        buys = defaultdict(dict)
+
+        for planet, planet_data in self.data.planets.items():
+            for resource, resource_data in planet_data.resources.items():
+                if resource_data.buy_price is not None:
+                    buys[resource][planet] = resource_data
+                if resource_data.sell_price is not None:
+                    sells[resource][planet] = resource_data
+
+        resources = sells.keys() & buys.keys()
+
+        best_buys = dict()
+        for resource in resources:
+            best_buy_price = float('inf')
+            best_buy_planet = None
+            for planet, resource_data in buys[resource].items():
+                if resource_data.buy_price < best_buy_price:
+                    best_buy_price = resource_data.buy_price
+                    best_buy_planet = planet
+            best_buys[resource] = {'planet': best_buy_planet, 'buy_price': best_buy_price}
+
+        best_sells = dict()
+        for resource in resources:
+            best_sell_price = float('inf')
+            best_sell_planet = None
+            for planet, resource_data in sells[resource].items():
+                if resource_data.sell_price < best_sell_price:
+                    best_sell_price = resource_data.sell_price
+                    best_sell_planet = planet
+            best_sells[resource] = {'planet': best_sell_planet, 'sell_price': best_sell_price}
+
+        best_deals = Counter({r: best_sells[r]['sell_price'] - best_buys[r]['buy_price'] for r in resources})
+        plan = []
+        for resource, profit in best_deals.most_common():
+            planet = best_buys[resource]['planet']
+            amount = buys[resource][planet].amount
+            plan.append((resource, planet, amount))
+
         commands = {}
         for ship_id, ship in my_ships.items():
+            if ship.ship_class != '3':
+                continue
             if ship.command is not None:
                 continue
+            if ship.resources.keys():
+                resource = list(ship.resources.keys())[0]
+                planet = best_sells[resource]['planet']
+                amount = ship.resources[resource]['amount']
+                if amount:
+                    commands[ship_id] = TradeCommand(target=planet, resource=resource, amount=-amount)
+                    continue
             random_planet_id = random.choice(list(self.data.planets.keys()))
             print(f"sending {ship_id} to {self.data.planets[random_planet_id].name}({random_planet_id})")
-            commands[ship_id] = MoveCommand(type="move", destination=Destination(target=random_planet_id))
+            # commands[ship_id] = MoveCommand(type="move", destination=Destination(target=random_planet_id))
+            resource, planet, amount = plan.pop(0)
+            ship_load = sum(r['amount'] for r in ship.resources.values())
+            # ship_free_capacity = ship.cargo_capacity - ship_load # todo must use common data
+            commands[ship_id] = TradeCommand(target=planet, resource=resource, amount=min(amount, 10))
 
-        pprint(commands) if commands else None
+        if commands:
+            print('Commands:')
+            pprint(commands)
         try:
             self.client.commands_post(commands)
         except ApiException as e:
